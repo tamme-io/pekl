17a5407956b0255e18dc2d902187a0dabf4d00c6
Jessica Glenn <jessica@skilld.com>
2017-08-25 04:01:26 +0000

    preparing to deploy as 0.03

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pekl',
-      version='0.02',
+      version='0.03',
       description='An AWS Lambda library for making invocations and responses with large bodies easier.',
       url='https://github.com/tamme-io/pekl',
       author='tamme',
